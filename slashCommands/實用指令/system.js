2f11d7dcca0660f27524ae0adef444fa35e88029
Night Cat <64583797+bengod1020@users.noreply.github.com>
2022-06-20 20:54:24 +0800

    Add existing file

--- slashCommands/實用指令/system.js
+++ slashCommands/實用指令/system.js
@@ -16,7 +16,11 @@ module.exports = {
     description: '查詢系統使用量',
     run: async (client, interaction, options) => {
         await interaction.deferReply().catch(e => { });
-        if(interaction.user.id !== "579544867626024960") return interaction.followUp("opps 這個指令只有夜貓能用喔awa");
+        function round(num) {
+            var m = Number((Math.abs(num) * 100).toPrecision(15));
+            return Math.round(m) / 100 * Math.sign(num);
+        }
+        
         function format(seconds){
             function pad(s){
               return (s < 10 ? '0' : '') + s;
@@ -34,16 +38,11 @@ module.exports = {
         const obj = Object.fromEntries(guildQueue);
         const size = Object.keys(obj).length;
         const osaa = require("os-utils");
-        function formatFloat(num, pos)
-        {
-        var size = Math.pow(10, pos);
-        return Math.round(num * size) / size;
-        }
         osaa.cpuUsage((v) => {
         const embed = new MessageEmbed()
         .setTitle("<:Discord_Bot:986319391660593172> 目前系統使用量:")
         .addField("<:cpu:986062422383161424> CPU型號:\n", `\`${cpu.brand}\``)
-        .addField("<:cpu:987630931932229632> CPU使用量:\n", `\`${formatFloat(v, 1)}\`%`, true)
+        .addField("<:cpu:987630931932229632> CPU使用量:\n", `\`${round(v) * 100}\`%`, true)
         .addField("<:rammemory:986062763598155797> RAM使用量:",`\`${usedRam}\\${totalRam}\` MB`, true)
         .addField("<:chronometer:986065703369080884> 運行時間:", `\`${format(uptime)}\``,true)
         .addField("<:server:986064124209418251> 總伺服器:",`\`${Clienta.guilds.cache.size}\``,true)
